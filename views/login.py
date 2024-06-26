97cc87a33c22564296eb8f40d7282990be2ba175
Flores Guerrero <vflores@BIOFLEXGROUP.NET>
2024-06-26 13:28:04 -0600

    login isterface

--- views/login.py
+++ views/login.py
@@ -25,10 +25,21 @@ ventana.geometry(f"{ancho_ventana}x{alto_ventana}+{posicion_x}+{posicion_y}")
 #Colocacion de nombre BIOFLEX
 etiqueta = tk.Label(ventana, text="BIOFLEX", fg="red", bg="white", font=("Lexend", 35), width=31, height=4, anchor="center")
 etiqueta.pack(pady=20)  # Ajusta el valor de `pady` según sea necesario
-etiqueta = tk.Label(ventana, text = "INGRESA USUARIO", fg="black", bg="white", font=("Arial", 20))
+etiqueta = tk.Label(ventana, text = "INGRESA USUARIO", fg="black", bg="white", font=("Arial", 20),width=30, height=1)
+etiqueta.pack()
+etiqueta
+
+#ejemplo de boton
+def obtener_texto():
+    texto_ingresado = cuadro_texto.get()
+    etiqueta.config(text="USUARIO: " + texto_ingresado)
+etiqueta = tk.Label(ventana, text="USUARIO: ")
 etiqueta.pack()
 
+cuadro_texto = tk.Entry(ventana, width=30)
+cuadro_texto.pack()
 
-
-
+boton = tk.Button(ventana, text="ENTRAR", command=obtener_texto)
+boton.pack()
+#
 ventana.mainloop()
