cbca0b42c6a7f230b27e3ac47dfd41b0193380b1
Cristián Giovanni Sanchez Gutierrez <csanchez@BFX-CAL-07.BIOFLEXGROUP.NET>
2024-07-04 14:00:36 -0600

    add

--- src/views/VentanaMain/pruebasFlet2.py
+++ src/views/VentanaMain/pruebasFlet2.py
@@ -1,13 +1,8 @@
 from typing import Any, List
 import flet as ft
-from flet import *          # Se importa todos los componentes de la Libreria "flet"
+from flet import *                                      # Se importa todos los componentes de la Libreria "flet"
+from src.views.VentanaMain.cmntsTbl import cntTable     # Componentes para el frame de la Tabla
 from src.Controllers.appTable import Controllers
-#from Controllers.appTable import Controllers
-
-def pru():
-    db = Controllers().get_row_Table()
-    print(db)
-    #pass
 
 class UI(UserControl):
     def __init__(self,page):
@@ -16,6 +11,7 @@ class UI(UserControl):
     # --- COMPONENTES ---
         self.page = page  
         self.color_teal = "teal"
+        self.tbl = cntTable(self.page)  # Se pasa el pagina actual
         self.dataTbl = Controllers()  #Accede a la información en la base de datos
     
         # --- INPUTS DE BUSQUEDA --- 
@@ -44,148 +40,7 @@ class UI(UserControl):
             label_style=TextStyle(color="black",italic=True)
         )
 
-        # --- TABLA ---
-            # --- Columnas de la tabla ---
-        self.Table = DataTable(
-            border= border.all(2,"purple"),
-            border_radius=5,
-            columns=[
-                DataColumn(Text("ID_PRODUCTO",color="whithe",weight="bold")),
-                DataColumn(Text("CLIENTE",color="whithe",weight="bold")),
-                DataColumn(Text("PRODUCTO",color="whithe",weight="bold")),
-                DataColumn(Text("FECHA",color="whithe",weight="bold")),
-                DataColumn(Text("HERRAMIENTAS",color="whithe",weight="bold"))
-            ],
-        )
-        # row table
-        self.showData() # Carga la función donde se recorre las tuplas de productos disponibles
-      
-    # -- Herramientas de la tabla --
-        # --- Delete Product ---
-    def dltButton(self,e):
-        idPrind = e.control.data[0]
-        self.mdlDlt = AlertDialog(
-            modal=True,
-            title=Text("Alerta!"),
-            content=Text(f"Estas Seguro de eliminar : {idPrind} ?"),
-            actions=[
-                #TextButton("Eliminar",on_click= lambda _: self.btnSlct(bnd=True,id=idPrind)),   # lambda _ : "_" el guión sirve para tomar ignorar los parametros, ya que no se usan en la función  
-                TextButton("Eliminar",on_click= lambda _: self.btnSlct(bnd=True,id=idPrind)),
-                TextButton("Cancelar",on_click= lambda _: self.btnSlct(bnd=False,id=None))
-            ],
-            actions_alignment= MainAxisAlignment.END
-        )
-        self.page.overlay.append(self.mdlDlt)
-        self.mdlDlt.open = True
-        self.page.update()
-        # ----
-
-        # -- Query Modal Delete --
-    def btnSlct(self,bnd,id):
-        if not bnd:
-            self.mdlDlt.open = False
-        else:
-            self.dataTbl.delete_row_Table(id=id)
-            self.mdlDlt.open = False
-            # -- Limpia y Actualiza la tabla -- 
-            self.Table.rows.clear() 
-            self.showData()
-            # Mensaje parte inferior
-            self.msgDlt = SnackBar(
-                content=Column(
-                    controls=[
-                        Container(
-                            Text(f"Se elimino : {id}",size=20,color="white"),
-                            alignment=alignment.center
-                        ),
-                    ],
-                ),
-                bgcolor="#831D1D",
-            )
-            self.page.overlay.append(self.msgDlt)
-            self.msgDlt.open = True 
-        self.page.update()
-        
         # -----------------------------
-
-    # --- BUSCADORES ---
-    def search_PrindCard(self,e):
-        srchPrindCrd = self.InptPrindCard.value.lower()
-        filterId = list(filter(lambda x: srchPrindCrd in x[0].lower(), self.dataTbl.get_row_Table()))
-        print("you find : ",filterId)
-        self.Table.rows = []
-
-        # Si el input es diferente de vacio
-        if not self.InptPrindCard.value == "":
-            # Si la list que se creo es mayor a cero, significa que encontro coincidencias
-            if len(filterId) > 0:
-                self.dataNotFound = False
-                for row in filterId:
-                    self.Table.rows.append(
-                        DataRow(
-                            cells=[
-                                DataCell(Text((row[0]))),
-                                DataCell(Text(row[1])),
-                                DataCell(Text(row[3])),
-                                DataCell(Text(row[2])),
-                                DataCell(
-                                    Row([
-                                        IconButton("delete",
-                                            #icons.CHECK,
-                                            icon_color="red",
-                                            data=row,
-                                            on_click=self.dltButton # --- PROXIMA TAREA ---
-                                        ),
-                                        IconButton("edit",
-                                            icon_color="green",
-                                            data=row,
-                                            #on_click=EditButton() # --- PROXIMA TAREA ---
-                                        )
-                                    ])
-                                )
-                            ]
-                        )
-                    )
-              
-                
-        # Componenete de Dato No Encontrado! para el Buscador de la tabla       
-        self.dataNotFound = Text("No se encontro el Producto!",
-                weight="bold",
-                size=20
-            )
-
-    # Muestra los datos de la base de datos
-    def showData(self):
-        self.Table.rows = []
-        for row in self.dataTbl.get_row_Table():    # Accede a la variable de la conexión
-            self.Table.rows.append(
-                DataRow(
-                    cells=[
-                        DataCell(Text((row[0]))),
-                        DataCell(Text(row[1])),
-                        DataCell(Text(row[3])),
-                        DataCell(Text(row[2])),
-                        DataCell(
-                            Row([
-                                IconButton("delete",
-                                    #icons.CHECK,
-                                    icon_color="red",
-                                    data=row,
-                                    on_click=self.dltButton # --- PROXIMA TAREA ---
-                                ),
-                                IconButton("edit",
-                                    icon_color="green",
-                                    data=row,
-                                    #on_click=EditButton() # --- PROXIMA TAREA ---
-                                )
-                            ])
-                        )
-                    ]
-                )
-            )
-        self.update()
-        
-    #-------------------------------------
 
     # --- CONTENEDORES,FRAMES ETC ... ---
         # --- FRAME FILTRADO ---
@@ -255,13 +110,13 @@ class UI(UserControl):
                     #self.data_table
                     ResponsiveRow(
                         controls=[
-                            self.Table
+                            self.tbl.Table
                         ]
                     )
-                   
                 ]
             )
         )
+      
 
         # Colocar los frames en forma de columna
         self.frameMain = Container(
@@ -271,10 +126,12 @@ class UI(UserControl):
             content=Column(
                     controls=[
                         self.cntFiltPrinf,
-                        self.cntTable
+                        self.tbl.cntTable
                     ],
                 ),
         )
+    # -------------------------------------
+
         # Hacer responsiva los framaes en forma de Fila
         self.container = ResponsiveRow(
             controls=[
@@ -292,6 +149,5 @@ def main(page: Page):       #   page : Es el Frame o la ventana de la Aplicació
     page.theme_mode = ThemeMode.DARK
     page.padding = 5
     page.add(UI(page))
-
 ft.app(main)
 #pru()
--- src/views/VentanaMain/vtnMain.py
+++ src/views/VentanaMain/vtnMain.py
@@ -1,88 +1,70 @@
+from flet import *
+
 import flet as ft
 
-class ejmpl(ft.UserControl):
-    def __init__(self):
-        super().__init__()
+class ejemplo(UserControl):
+    def __init__(self,page):
+        super().__init__(expand=True)
 
-    def build(self):
-        title = "AlertDialog examples"
-        horizontal_alignment = ft.CrossAxisAlignment.CENTER
+        self.page = page
 
-        self.dlg = ft.AlertDialog(
-            title=ft.Text("Hi, this is a non-modal dialog!"),
-            on_dismiss=lambda e: self.page.add(ft.Text("Non-modal dialog dismissed")),
-        )
 
-        self.dlg_modal = ft.AlertDialog(
-            modal=True,
-            title=ft.Text("Please confirm"),
-            content=ft.Text("Do you really want to delete all those files?"),
-            actions=[
-                ft.TextButton("Yes", on_click=self.handle_close),
-                ft.TextButton("No", on_click=self.handle_close),
-            ],
-            actions_alignment=ft.MainAxisAlignment.END,
-            on_dismiss=lambda e: self.page.add(
-                ft.Text("Modal dialog dismissed"),
-            ),
-        )
-
-        # Botones que abren el modal
-        self.cntIni = ft.Container(
-            content=ft.Column(
+        self.frame1 = Container(
+            bgcolor="red",
+            width=150,
+            height=150,
+            padding=5,
+            margin=0,
+            alignment=alignment.center,
+            content= Row(
                 controls=[
-                    ft.ElevatedButton("Open dialog", on_click=self.open_non_modal_dialog),
-                    ft.ElevatedButton("Open modal dialog", on_click=self.open_modal_dialog),
-                ]
-            ),
-            alignment=ft.alignment.center
+                    Text("Hola"),
+                    Text("Hola"),
+                    Text("Hola"),
+                ],
+            )
         )
 
-        self.ejemplo = ft.Container(
+        self.frame2 = Container(
+            bgcolor="blue",
+            width=150,
+            height=150,
+            padding=10,
+            margin=0,
+            alignment=alignment.center,
+            content= Row(
+                controls=[
+                    Text("Hola"),
+                    Text("Hola"),
+                    Text("Hola"),
+                ],
+            )
+        )
+
+    def ejemplo(self,e):
+        pass
+
+    # Colocar los frames en forma de columna
+        self.frameMain = Container(
+            bgcolor="yellow",
             border_radius=10,
-            width=100,
-            height=100,
-            shadow=ft.BoxShadow(
-                spread_radius=0,
-                blur_radius=50,
-                color=ft.colors.AMBER_700,
-                offset=ft.Offset(50, 110),
-                blur_style=ft.ShadowBlurStyle.OUTER,
-            )
+            padding=2,
+            content=Column(
+                    expand=False,
+                    controls=[
+                        self.frame1,
+                        self.frame2
+                    ],
+                ),
         )
-
-        # Frame Principal
-        self.frameMain = ft.Container(
-            content=ft.Column(
-                controls=[
-                    self.cntIni,
-                    self.ejemplo
-                ]
-            )
-        )
-
+        
+    # Construye todos los frames tiene el frame Main
+    def build(self):
         return self.frameMain
-    def pruebas(self):
-        self.pr = "Messasge!"
 
-    def open_non_modal_dialog(self, e):
-        self.page.overlay.append(self.dlg)
-        self.dlg.open = True
-        self.page.update()
+def main(page: Page):
+    page.theme_mode = ThemeMode.DARK
+    page.add(ejemplo(page))
 
-    def open_modal_dialog(self, e):
-        self.page.overlay.append(self.dlg_modal)
-        self.dlg_modal.open = True
-        self.page.update()
-
-    def handle_close(self, e):
-        self.dlg_modal.open = False
-        self.page.update()
-        self.page.add(ft.Text(f"Modal dialog closed with action: {e.control.text}"))
-
-def main(page: ft.Page):
-    page.padding = 5
-    page.add(ejmpl())
-
-ft.app(target=main)
-
+# Main
+app(main)
