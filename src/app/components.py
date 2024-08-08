31027913976ad0bdf0b65bded40dbfc20a1ca736
Cristián Giovanni Sanchez Gutierrez <csanchez@BFX-CAL-07.BIOFLEXGROUP.NET>
2024-08-08 14:32:15 -0600

    add

--- src/app/components.py
+++
@@ -1,6 +0,0 @@
-def addFichTec(data,c):
-    insertQuery = f"""
-        INSERT INTO FichaTec(id_codProduct, cliente, fecha_Elav, fecha_Rev, producto)
-        VALUES('{data[8][c]}', '{data[6][c]}', 'N/A', '{data[2][c]}', '{data[7][c]}')
-    """
-    return insertQuery    
